9b4db6a3c873c02f28a2fa38a57c7f5a274ceeba
yudenichaa <yudenichalexandr@yandex.ru>
2020-11-04 18:04:36 +0300

    made title in cases by country not scrollable

--- src/components/CasesByCountry/CasesByCountry.js
+++ src/components/CasesByCountry/CasesByCountry.js
@@ -14,25 +14,27 @@ export default function CasesByCountry({ countries, displayInfo }) {
     return (
         <div className="cases-by-country">
             <h2 className="cases-by-country__headline">Total by country</h2>
-            <table className="cases-by-country__table">
-                <tbody>
-                    {sortedCountries.map((country) => {
-                        return (
-                            <tr
-                                className="cases-by-country__table-row"
-                                key={country.country}
-                            >
-                                <td>{country.country}</td>
-                                <td>
-                                    {numeral(country[displayInfo]).format(
-                                        "0,00a"
-                                    )}
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            <div className="cases-by-country__table-container">
+                <table className="cases-by-country__table">
+                    <tbody>
+                        {sortedCountries.map((country) => {
+                            return (
+                                <tr
+                                    className="cases-by-country__table-row"
+                                    key={country.country}
+                                >
+                                    <td>{country.country}</td>
+                                    <td>
+                                        {numeral(country[displayInfo]).format(
+                                            "0,00a"
+                                        )}
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
